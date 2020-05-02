62a33c740e6781c0b39eae21cfeeab205228833a
alexis.wardmar <alexis.wardmar@gmail.com>
2020-05-02 03:41:02 +0200

    commentaire pour Wbads.py, google.py et resume.py

--- google.py
+++ google.py
@@ -1,14 +1,24 @@
 from requests_html import HTMLSession
 
+
+# Ouverture d'une session 
 session = HTMLSession()
 
+# Le film à rechercher
 chaine = "la ligne verte"
-url = "https://www.google.com/search?q="
+
+# Remplacement des espace par des +
 chaine = (chaine.strip()).replace(" ","+")
+
+# Url de base pour une recherche Google
+url = "https://www.google.com/search?q="
+
+# Création de la requette
 url += chaine +"+allocine"
 
-
+# Génération de la page
 r = session.get(url)
 
+# Création d'un tableau contenant l'url du film sur allociné
 fiche = r.html.xpath("//div[@class = 'g'][1]//div[@class = 'r'][1]/a/@href")
 print(fiche)
